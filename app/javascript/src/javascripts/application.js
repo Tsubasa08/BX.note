d0668663fc5a015fad121e49a67c417c6ebc8c9c
tsubasa.masaki <tsubasa.masaki@tsubasamasakinoMacBook-puro.local>
2019-06-13 07:47:39 +0900

    ヘッダー作成

--- app/javascript/src/javascripts/application.js
+++ app/javascript/src/javascripts/application.js
@@ -1,5 +1,15 @@
 $(function() {
+  // ハンバーメニュー
   $(".nav-toggle").click(function() {
     $("body").toggleClass("nav-open");
+  });
+
+  // 検索ページ遷移
+  $("#sp-serch").click(function() {
+    $("#serch-page").show();
+  });
+
+  $("#serch-page-close").click(function() {
+    $("#serch-page").hide();
   });
 });
