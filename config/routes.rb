d85595ec034cc204f419622ecc4d64571dbe2b70
tsubasa.masaki <tsubasa.masaki@tsubasamasakinoMacBook-puro.local>
2019-06-14 18:20:24 +0900

    Userモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
   root 'static_pages#top'
 
   get    '/about',   to: 'static_pages#about'
   get    '/terms',   to: 'static_pages#terms'
   get    '/policy',  to: 'static_pages#policy'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 end
