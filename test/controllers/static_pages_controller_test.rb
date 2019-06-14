9fade5c2edab38be969ae1c02ef2b6c524147522
tsubasa.masaki <tsubasa.masaki@tsubasamasakinoMacBook-puro.local>
2019-06-14 15:40:28 +0900

    固定ページテストコード作成

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "BX.note"
+  end
+
   test "should get top" do
-    get static_pages_top_url
+    get root_path
     assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "BX.noteとは | #{@base_title}"
+  end
+
+  test "should get terms" do
+    get terms_path
+    assert_response :success
+    assert_select "title", "利用規約 | #{@base_title}"
+  end
+
+  test "should get policy" do
+    get policy_path
+    assert_response :success
+    assert_select "title", "プライバシーポリシー | #{@base_title}"
   end
 
 end
