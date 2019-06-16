7d99ded3b3f88c3337690c6a163987b30031852a
tsubasa.masaki <>
2019-06-17 07:14:41 +0900

    フォーム 編集

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+  
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: "",
+                                 email: "user@invalid",
+                                 password: "foo",
+                                 password_confirmation: "bar"} }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'li', "Name can't be blank"
+    assert_select 'li', "Email is invalid"
+    assert_select 'li', "Password is too short (minimum is 6 characters)"
+    assert_select 'li', "Password confirmation doesn't match Password"
+    assert_select 'form[action="/signup"]'
+  end
+
+  test "valid signup information with account activation" do
+    get signup_path
+     assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                  email: "user@example.com",
+                                  password: "password",
+                                  password_confirmation: "password"} }
+    end
+    # assert_equal 1, ActionMailer::Base.deliveries.size
+    # user = assigns(:user)
+    # # 有効化していない状態でログイン
+    # log_in_as(user)
+    # assert_not is_logged_in?
+    # # 有効化トークンが不正な場合
+    # get edit_account_activation_path("invalid token", email: user.email)
+    # assert_not is_logged_in?
+    # # トークンは正しいがメールアドレスが無効な場合
+    # get edit_account_activation_path(user.activation_token, email: 'wrong')
+    # # 有効化トークンが正しい場合
+    # get edit_account_activation_path(user.activation_token, email: user.email)
+    # assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div.alert-success'
+    # assert is_logged_in?
+  end
+
+end
