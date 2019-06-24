328b0fc9aada36e3e2626c35711261687cf0fcde
tsubasa.masaki <>
2019-06-24 14:50:17 +0900

    Posts コントローラー編集 投稿表示レイアウト表示

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,5 @@ Rails.application.routes.draw do
   resources :users
 
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :posts,          only: [:create, :destroy]
 end
