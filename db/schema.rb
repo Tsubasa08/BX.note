20b0517543564d18eb930dbc78f546321e177241
tsubasa.masaki <>
2019-06-27 08:49:03 +0900

    Post model categoryカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_220607) do
+ActiveRecord::Schema.define(version: 2019_06_26_234358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2019_06_25_220607) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
-    t.string "category"
     t.string "article_url"
     t.string "book_title"
     t.integer "book_evaluation"
