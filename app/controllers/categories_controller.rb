43b7d8b6a35a9da847ad350a1362088bd8e2af2e
tsubasa.masaki <>
2019-06-27 08:16:03 +0900

    カテゴリー レイアウト編集

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,7 @@
 class CategoriesController < ApplicationController
 
   def show
+    @categories = Category.all
     @category = Category.find(params[:id])
-    @id = @category.id
-    # @posts = @category.post_categories
-    # @posts = Post_Categories.where(category_id: @id)
-    # @posts = @category.post_categories.build
-     @category.post_categories.each do |po| 
-       @posts = po.post
-     end 
-    #  @posts = @category.post_categories
-    # @posts = current_user.posts.build if logged_in?
   end
 end
