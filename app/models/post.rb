ad72349f49a6f9f183e09d388af0b114cc62d0e4
tsubasa.masaki <>
2019-07-06 13:04:17 +0900

    Like model作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :categories, through: :post_categories
   belongs_to :user
   has_many_attached :images
+  has_many :likes, dependent: :destroy
+  
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
   # has_many :comments, dependent: :destroy
-  # has_many :likes, dependent: :destroy
   # フォローしている相手を関連付ける
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
