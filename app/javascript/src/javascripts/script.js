27851d7b930f03652ce8790baa6307a119d43e6e
tsubasa.masaki <>
2019-07-10 21:20:51 +0900

    投稿検索結果 JS修正

--- app/javascript/src/javascripts/script.js
+++ app/javascript/src/javascripts/script.js
@@ -137,10 +137,10 @@ $(function() {
   });
 
   // 投稿検索結果 一致する結果なし
-  let posts = $(".post-list").children();
+  let posts = $("#post-list--search").children();
   if (posts.length === 0) {
     let title = $("#search-title").text();
-    $(".post-list").append(
+    $("#post-list--search").append(
       `<p class="failure-text">「${title}」に一致する検索結果はありません</p>`
     );
   }
