65b0bd80c999d699af929cdc3a982bc2f4fa8c16
tsubasa.masaki <>
2019-07-12 06:57:46 +0900

    レイアウト表示修正

--- test/integration/posts_interface_test.rb
+++ test/integration/posts_interface_test.rb
@@ -37,6 +37,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # assert_select 'a', text: 'delete', count: 0
   end
 
+
   test "genre: article 統合的投稿" do
     log_in_as(@user)
     post select_path, params: { data: "article"}, xhr: true #Ajax通信
@@ -62,6 +63,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     follow_redirect!
   end
 
+
   test "genre: book 統合的投稿" do
     log_in_as(@user)
     post select_path, params: { data: "book"}, xhr: true #Ajax通信
@@ -72,34 +74,46 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # 無効な送信
     content = "お腹すいた"
     assert_no_difference 'Post.count' do
-      post posts_path, params: { post: { content: content, genre: "book", link_url: "https://qiita.com/"  } }
+      post posts_path, params: { post: { content: content, genre: "book", link_url: "https://qiita.com/",
+                                         book_evaluation: ""  } }
     end
     # assert_select 'div#error_explanation'
     # 有効な送信
+    #本検索(nokogiri)
+    book_title = "html"
+    link_url = "https://www.amazon.co.jp/s?k=#{book_title}&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&ref=nb_sb_noss_2"
+    link_title = "1冊ですべて身につくHTML & CSSとWebデザイン入門講座"
+    link_image = "https://m.media-amazon.com/images/I/813E7x9kz7L._AC_UL320_.jpg"
+    book_evaluation = 3
+    get search_book_path, params: { content: book_title }, xhr: true
+    # assert_equal "https://www.amazon.co.jp/s?k=#{book_title}&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&ref=nb_sb_noss_2",url 
+    assert_not $doc.nil?
     assert_difference 'Post.count', 1 do
-      post posts_path, params: { post: { content: content, genre: "book", link_url: "https://qiita.com/"  } }
+      post posts_path, params: { post: { content: content, genre: "book", link_url: link_url, link_title: link_title, link_image: link_image, book_evaluation: book_evaluation} }
     end
-    @post = Post.first
-    assert_equal "プログラマの技術情報共有サービス - Qiita", @post.link_title
-    assert_equal "Qiitaは、プログラマのための技術情報共有サービスです。 プログラミングに関するTips、ノウハウ、メモを簡単に記録 &amp; 公開することができます。", @post.link_desc
-    assert_equal "https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png", @post.link_image
     assert_redirected_to root_url
     follow_redirect!
   end
 
-  # サイドバーのマイクロポストの投稿数をテスト
-  # test "micropost sidebar count" do
-  #   log_in_as(@user)
-  #   get root_path
-  #   assert_match "#{@user.microposts.count} microposts", response.body
-  #   # まだマイクロポストを投稿していないユーザー
-  #   other_user = users(:malory)
-  #   log_in_as(other_user)
-  #   get root_path
-  #   assert_match "0 microposts", response.body
-  #   other_user.microposts.create!(content: "A micropost")
-  #   get root_path
-  #   assert_match "1 micropost", response.body
-  # end
+  
+  test "ユーザーページ 投稿数カウント" do
+    log_in_as(@user)
+    get user_path(@user)
+    assert_select "a.post-page-link" do
+      assert_select "span[class=link__num]", text: "#{@user.posts.count}"
+    end
+    # まだマイクロポストを投稿していないユーザー
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get user_path(other_user)
+    assert_select "a.post-page-link" do
+      assert_select "span[class=link__num]", text: "0"
+    end
+    other_user.posts.create!(content: "知らない天井", genre: "other")
+    get user_path(other_user)
+    assert_select "a.post-page-link" do
+      assert_select "span[class=link__num]", text: "#{other_user.posts.count}"
+    end
+  end
 
 end
