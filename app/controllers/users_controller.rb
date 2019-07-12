14b29beb28fc8ecda791d096af8f347c9725e906
tsubasa.masaki <>
2019-07-12 14:21:02 +0900

    user 管理者権限(admin: true)削除機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 before_action :logged_in_user, only: [:edit, :update, :destroy]
 before_action :correct_user, only: [:edit, :update]
-# before_action :admin_user, only: :destroy
+before_action :admin_user, only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -42,8 +42,8 @@ before_action :correct_user, only: [:edit, :update]
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
+    flash[:success] = "ユーザーを削除しました。"
+    redirect_to root_url
   end
 
   def following
@@ -82,9 +82,9 @@ before_action :correct_user, only: [:edit, :update]
   end
 
   # 管理者かどうか確認
-  # def admin_user
-  #   redirect_to(root_url) unless current_user.admin?
-  # end
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
   
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -38,29 +38,37 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
-  # test "should not allow the admin attribute to be edited via the web" do
-  #   log_in_as(@other_user)
-  #   assert_not @other_user.admin?
-  #   patch user_path(@other_user), params: { user: { password: @other_user.password, 
-  #                                                   password_confirmation: @other_user.password,
-  #                                                   admin: true} }
-  #   assert_not @other_user.reload.admin?
-  # end
+  test "admin属性の変更" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { password: @other_user.password, 
+                                                    password_confirmation: @other_user.password,
+                                                    admin: true} }
+    assert_not @other_user.reload.admin?
+  end
 
-  test "should redirect destroy when not logged in" do
+  test "destroyアクスション ログインなし" do
     assert_no_difference 'User.count' do
      delete user_path(@user)
     end
     assert_redirected_to login_url
   end
 
-  # test "should redirect destroy when logged in as a non-admin" do
-  #   log_in_as(@other_user)
-  #   assert_no_difference 'User.count' do
-  #     delete user_path(@user)
-  #   end
-  #   assert_redirected_to root_url
-  # end
+  test "destroyアクスション admin属性:false" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+  test "destroyアクスション admin属性:true" do
+    log_in_as(@user)
+    assert_difference 'User.count', -1 do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 
 
 
