f503e1db19ee982d191e7e5dfb00b2a58053d416
tsubasa.masaki <>
2019-07-15 20:11:41 +0900

    投稿モーダル 発火条件 checkbox → click

--- app/javascript/src/javascripts/scroll.js
+++ app/javascript/src/javascripts/scroll.js
@@ -7,22 +7,73 @@ $(function() {
     topPosition = $(window).scrollTop(); // スクロールトップポジションを取得
   });
 
+  // let nowPositions = () => {
+  //   let topPosition, nowPosition;
+  //   $(window).scroll(function() {
+  //     topPosition = $(window).scrollTop(); // スクロールトップポジションを取得
+  //   });
+  //   nowPosition = topPosition;
+  //   return nowPosition;
+  // };
+  // console.log(nowPositions());
+
   // 投稿モーダル 表示
-  $("#modal-checkbox--post").change(function() {
+  $("#modal-open").click(function() {
+    nowPosition = topPosition; //クリック時の位置取得
+    $("body").toggleClass("active");
     if (windowWidth >= breakPoint) {
-      nowPosition = topPosition; //クリック時の位置取得
+      $("#modal-content--post, #modal-close--post").fadeIn();
       $("#modal-content--post").css("top", nowPosition + 50);
       $("#modal-close--post").css("top", nowPosition);
+      $("#modal-close--post").addClass("active");
     } else {
-      nowPosition = topPosition; //クリック時の位置取得
-      $("#modal-close--post, #modal-content--post").css("top", nowPosition);
-      $("main").hide();
-    }
-    $("body").toggleClass("active");
-    if ($(this).prop("checked") == true) {
       $("#modal-content--post").fadeIn();
-    } else {
-      $("#modal-content--post").fadeOut("fast");
+
+      $("#modal-close--post, #modal-content--post").css("top", 0);
+      $("main, footer").hide();
     }
   });
+
+  // -- モーダルを閉じる --
+  const closeModal = function() {
+    $("#modal-content--post, #modal-close--post").fadeOut("fast");
+    $("body").removeClass("active");
+    $(window).scrollTop(nowPosition); //クリック時の位置
+    console.log(nowPosition);
+  };
+  $("#close-btn--tab").click(function() {
+    $("main, footer").show();
+    closeModal();
+  });
+  $(document).on("click touchend", function(event) {
+    if (!$(event.target).closest("#modal-content--post, #modal-open").length) {
+      closeModal();
+    }
+  });
+
+  // // 投稿モーダル 表示
+  // $("#modal-checkbox--post").change(function() {
+  //   $(window).scroll(function() {
+  //     topPosition = $(window).scrollTop(); // スクロールトップポジションを取得
+  //   });
+  //   const nowPosition = topPosition; //クリック時の位置取得
+  //   if (windowWidth >= breakPoint) {
+  //     $("#modal-content--post").css("top", nowPosition + 50);
+  //     $("#modal-close--post").css("top", nowPosition);
+  //   } else {
+  //     $("#modal-close--post, #modal-content--post").css("top", 0);
+  //     $("main, footer").hide();
+  //   }
+  //   $("body").toggleClass("active");
+  //   if ($(this).prop("checked") == true) {
+  //     $("#modal-content--post").fadeIn();
+  //   } else {
+  //     $("#modal-content--post").fadeOut("fast");
+  //     console.log(nowPosition);
+  //     if (windowWidth < breakPoint) {
+  //       $(window).scrollTop(nowPosition); //クリック時の位置
+  //       $("main, footer").show();
+  //     }
+  //   }
+  // });
 });
