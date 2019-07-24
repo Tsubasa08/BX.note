5e8e7940b5f7f7bf47a5a2166518c6f0b9ceb73e
tsubasa.masaki <>
2019-07-24 15:52:59 +0900

    【コード整形】helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,32 +11,10 @@ module ApplicationHelper
   end
 
   # ホスト名取得
-  # def get_host(path)
-  #   host = path.sub(/\\/, '/').match(/\/\/([^\/]*)/)
-  #   host[1]
-  # end
-
   def get_host(path)
   require 'uri'
   uri = URI.parse(path)
   uri.host
 end
-
-  # 記事のOGP取得
-  # def get_ogp(url, data)
-  #   require 'nokogiri'
-  #   require 'open-uri'
-  #   $doc = Nokogiri::HTML(open(url))
-  #   $data = data
-
-  #   if data = "title"
-  #     ogp = $doc.css('meta[property="og:title"]').attribute('content').to_s
-  #   elsif data = "image"
-  #     ogp = $doc.css('meta[property="og:image"]').attribute('content').to_s
-  #   elsif data = "desc"
-  #     ogp = $doc.css('meta[property="og:description"]').attribute('content').to_s
-  #   end
-  #   ogp
-  # end
 
 end
