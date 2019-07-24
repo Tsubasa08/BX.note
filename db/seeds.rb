bea39ab89fab62c2d3de8774d38eafcb8272c690
tsubasa.masaki <>
2019-07-25 08:52:07 +0900

    テストユーザー作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-User.create!(name:  "Example User",
-             email: "example@mail.com",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
+User.create!(name:  "kaerukun@test",
+             email: "kaeru@mail.com",
+             password:              "password",
+             password_confirmation: "password",
+             introduce: "テストユーザーです。よろしくお願いします。")
 
 
 %W[HTML CSS  JavaScript jQuery WordPress デザイン Web制作].each { |a| Category.create(name: a) }
